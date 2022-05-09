a845fe3d90ca0ccf802913fa9f9ce476ecd528b1
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-05-09 16:17:39 +0200

    Create theme toggle

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,32 @@
 import AddTask from "./components/AddTask";
 import light from "./images/bg-mobile-light.jpg";
-import lightIcon from "./images/icon-moon.svg";
+import dark from "./images/bg-mobile-dark.jpg";
+import lightIcon from "./images/icon-sun.svg";
+import darkIcon from "./images/icon-moon.svg";
+import { useState } from "react";
 function App() {
+  const [theme, setTheme] = useState(false);
   return (
-    <main className="w-full h-screen relative">
+    <main
+      className={`w-full h-screen relative ${
+        theme ? "bg-veryLightGray" : "bg-veryDarkBlue"
+      }`}
+    >
       <div>
-        <img src={light} alt="banner" />
+        <img src={theme ? light : dark} alt="banner" />
       </div>
       <div className="absolute top-12 left-6 right-6">
         <header className="flex justify-between text-veryLightGray text-3xl">
           <h2>TODU</h2>{" "}
           <div>
-            <img src={lightIcon} alt="toggle" />
+            <img
+              src={theme ? lightIcon : darkIcon}
+              alt="toggle"
+              onClick={() => setTheme((prev) => !prev)}
+            />
           </div>
         </header>
-        <AddTask />
+        <AddTask theme={theme} />
       </div>
     </main>
   );
--- client/src/components/AddTask.js
+++ client/src/components/AddTask.js
@@ -1,16 +1,25 @@
 import React from "react";
 
-const AddTask = () => {
+const AddTask = ({ theme }) => {
   return (
     <div
-      className="flex mt-6 bg-veryLightGray p-3 rounded-md space-x-3 items-center
-    "
+      className={`flex mt-6 ${
+        theme ? "bg-veryLightGray" : "bg-veryDarkBlue"
+      } p-3 rounded-md space-x-3 items-center`}
     >
-      <div className="w-6 h-6 ml-3 border-2 rounded-full"></div>
+      <div
+        className={`w-6 h-6 ml-3 border rounded-full ${
+          theme ? "border-darkGrayishBlue" : "border-lightGrayishBlue"
+        }`}
+      ></div>
       <input
         type="text"
         placeholder="Create a new todu..."
-        className="border-none focus:outline-none bg-veryLightGray text-sm"
+        className={`border-none focus:outline-none bg-transparent text-sm ${
+          theme
+            ? "placeholder:text-darkGrayishBlue"
+            : "placeholder:text-lightGrayishBlue"
+        }`}
       />
     </div>
   );
